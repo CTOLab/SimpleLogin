bf3b218bf2712206ff51837c5b959e9ac00e7ad8
Michael Fussenegger <michaelfussenegger@td-12017-mac.td-berlin.local>
2015-01-08 17:00:24 +0100

    adding Outlets

--- SimpleLogin/ViewController.swift
+++ SimpleLogin/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var textUserName: UITextField!
+    @IBOutlet var textPwd: UITextField!
+    @IBOutlet var buttonSubmit: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
