d6197d8efb58a6b6f1d89dfa13322af2708226f1
Michael Fussenegger <michaelfussenegger@td-12017-mac.td-berlin.local>
2015-01-08 15:49:46 +0100

    Initial Commit

---
+++ SimpleLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleLogin
+//
+//  Created by Michael Fussenegger on 08.01.15.
+//  Copyright (c) 2015 Michael Fussenegger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
